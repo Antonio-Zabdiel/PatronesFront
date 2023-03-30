fcd443bb49de52646c5751d54b6537d7e54c342b
martin-sainos-demian <martin.sainos.demian@gmail.com>
2023-03-30 14:31:02 -0600

    innerHTML

--- js/logic.js
+++ js/logic.js
@@ -2,6 +2,7 @@ const llamarAPI = async () => {
     const response = await fetch('https://talented-fox-panama-hat.cyclic.app/')
     const data = await response.json()
     console.log(data)
+    document.getElementById("respuesta").innerHTML = data
 }
 
 document.getElementById("llamar").onclick = () => {
